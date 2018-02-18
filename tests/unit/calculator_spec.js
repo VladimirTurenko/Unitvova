a16166adc1770f0374a7c0b02cb792944a26756b
RunsWritesCode <sophieamullins@gmail.com>
2018-02-18 15:43:19 +0000

    comments removed

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -12,13 +12,15 @@ describe('calculator', function () {
   })
 
   it('should be able to add', function() {
+    calculator.previousTotal = 4;
     calculator.add(4);
-    assert.equal(calculator.runningTotal, 4);
+    assert.equal(calculator.runningTotal, 8);
   })
 
   it('should be able to subtract', function() {
+    calculator.previousTotal = 4;
     calculator.subtract(2);
-    assert.equal(calculator.runningTotal, -2);
+    assert.equal(calculator.runningTotal, 2);
   })
 
   it('should be able to multiply', function() {
@@ -33,21 +35,33 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, 2);
   })
 
+  it('should be able to concatenate the clicked number to the running total', function() {
+    calculator.numberClick(4);
+    calculator.numberClick(2);
+    assert.equal(calculator.runningTotal, 42);
+  })
+
   it('should be able to clear running total on number click', function() {
-    calculator.add(4);
-    calculator.add(4);
-    assert.equal(calculator.runningTotal, 4);
+    calculator.runningTotal = 42;
+    calculator.numberClick(2)
+    assert.equal(calculator.runningTotal, 2);
   })
 
   it('should use operator clicked', function() {
     calculator.operatorClick('+');
-    assert.equal(true, calculator.newTotal);
+    assert.equal(calculator.newTotal, true);
+  })
+
+  it('should be able to clear total when clearClick is called', function() {
+    calculator.runningTotal = 42;
+    calculator.clearClick();
+    assert.equal(calculator.runningTotal, 0);
   })
 
   it('should be able to clear previousOperator', function(){
-  calculator.previousOperator = '='
-  calculator.clearClick()
-  assert.equal(null, calculator.previousOperator)
+  calculator.previousOperator = '=';
+  calculator.clearClick();
+  assert.equal(calculator.previousOperator, null);
 })
 
 
