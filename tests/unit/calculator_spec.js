2b5931497c677cfa7cb94ec62e9c8579201fed49
turenkovn <75043951+VladimirTurenko@users.noreply.github.com>
2022-05-22 20:09:25 +0300

    Update calculator_spec.js

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -16,11 +16,41 @@ describe('calculator', function () {
     calculator.add(4);
     assert.equal(calculator.runningTotal, 8);
   })
+  
+   it('Limit value check 0', function() {
+    calculator.previousTotal = 0;
+    calculator.add(0);
+    assert.equal(calculator.runningTotal, 0);
+  })
+  
+  it('Limit value check left 0', function() {
+    calculator.previousTotal = 0;
+    calculator.add(5);
+    assert.equal(calculator.runningTotal, 5);
+  })
+  
+  it('Limit value check right 0', function() {
+    calculator.previousTotal = 6;
+    calculator.add(0);
+    assert.equal(calculator.runningTotal, 6);
+  })
+  
+   it('Checking large limit values', function() {
+    calculator.previousTotal = 125;
+    calculator.add(125);
+    assert.equal(calculator.runningTotal, 250);
+  })
 
   it('should be able to subtract', function() {
     calculator.previousTotal = 4;
     calculator.subtract(2);
     assert.equal(calculator.runningTotal, 2);
+  })
+  
+  it('Subtraction check 0', function() {
+    calculator.previousTotal = 4;
+    calculator.subtract(0);
+    assert.equal(calculator.runningTotal, 0);
   })
 
   it('should be able to multiply', function() {
@@ -28,11 +58,23 @@ describe('calculator', function () {
     calculator.multiply(2);
     assert.equal(calculator.runningTotal, 4);
   })
+  
+   it('Checking multiplication by 0', function() {
+    calculator.previousTotal = 2;
+    calculator.multiply(0);
+    assert.equal(calculator.runningTotal, 0);
+  })
 
   it('should be able to divide', function() {
     calculator.previousTotal = 8;
     calculator.divide(4);
     assert.equal(calculator.runningTotal, 2);
+  })
+  
+  it('Verification Zero is divided by the number', function() {
+    calculator.previousTotal = 0;
+    calculator.divide(4);
+    assert.equal(calculator.runningTotal, 0);
   })
 
   it('should be able to concatenate the clicked number to the running total', function() {
@@ -47,8 +89,13 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, 2);
   })
 
-  it('should use operator clicked', function() {
+  it('should use operator clicked +', function() {
     calculator.operatorClick('+');
+    assert.equal(calculator.newTotal, true);
+  })
+  
+   it('should use operator clicked -', function() {
+    calculator.operatorClick('-');
     assert.equal(calculator.newTotal, true);
   })
 
@@ -57,6 +104,12 @@ describe('calculator', function () {
     calculator.clearClick();
     assert.equal(calculator.runningTotal, 0);
   })
+  
+  it('should be able to clear total when clearClick is called =0', function() {
+    calculator.runningTotal = 0;
+    calculator.clearClick();
+    assert.equal(calculator.runningTotal, 0);
+  })  
 
   it('should be able to clear previousOperator', function(){
   calculator.previousOperator = '=';
